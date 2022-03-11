2341f31fc6a094459c04e8afde47004f691e3fe6
Pandyakuza <adam.marie4869@hotmail.com>
2022-03-11 14:59:12 +0100

    Sticky Header JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,16 @@ document.addEventListener ("DOMContentLoaded", () =>{
             preloader.remove();
         });
     }
+
+
+    /** ----------------------------------Sticky NavBar code ------------------------------------*/
+
+    const selectHeader = document.querySelector("#header");
+    if (selectHeader){
+        document.addEventListener("scroll", () =>{
+            window.scrollY > 100
+            ? selectHeader.classList.add("sticked")
+            :selectHeader.classList.remove("sticked")
+        });
+    }
 });
